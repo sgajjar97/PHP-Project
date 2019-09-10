99bcae27756194a71f7ac891f445e9b42a5a5803
Shail Gajjar <sgajjar97@gmail.com>
2019-09-10 16:46:03 -0700

    Add files via upload

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -2,13 +2,32 @@
 
 <html lang="en">
   <head>
-    <title>Globe Bank</title>
+    <title>GBI</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/staff.css">
   </head>
 
   <body>
 
-  	<h1>Globe Bank coming soon</h1>
+  	<header>
+  		<h1>GBI Staff Area</h1>
+  	</header>
+
+  	<navigation>
+  		<ul>
+  			<li><a href="index.php">Menu</a></li>
+  		</ul>
+  	</navigation>
+
+  	<div id="content">
+  		
+  	</div>
+
+  	<footer>
+  		&copy; <?php echo date('Y'); ?> Globe Bank
+  	</footer>
+
+
 
   </body>
 </html>
