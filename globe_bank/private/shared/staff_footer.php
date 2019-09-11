d90cb765a77a6a825e3ac5b6517b996859cc0e50
Shail Gajjar <sgajjar97@gmail.com>
2019-09-10 17:34:16 -0700

    Add files via upload

---
+++ globe_bank/private/shared/staff_footer.php
@@ -0,0 +1,8 @@
+<footer>
+  		&copy; <?php echo date('Y'); ?> Globe Bank
+  	</footer>
+
+
+
+  </body>
+</html>
--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,33 +1,11 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../stylesheets/staff.css">
-  </head>
-
-  <body>
-
-  	<header>
-  		<h1>GBI Staff Area</h1>
-  	</header>
-
-  	<navigation>
-  		<ul>
-  			<li><a href="index.php">Menu</a></li>
-  		</ul>
-  	</navigation>
+<?php include('../../private/shared/staff_header.php'); ?>
 
   	<div id="content">
   		
   	</div>
 
-  	<footer>
-  		&copy; <?php echo date('Y'); ?> Globe Bank
-  	</footer>
+<?php include('../../private/shared/staff_footer.php'); ?>
 
-
-
-  </body>
-</html>
+  	
