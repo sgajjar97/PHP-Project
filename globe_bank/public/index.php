ea0063810abe26f394e2a08e2f12bbf3c2a825cf
Shail Gajjar <sgajjar97@gmail.com>
2019-09-27 16:41:45 -0700

    Add files via upload

--- globe_bank/public/index.php
+++ globe_bank/public/index.php
@@ -2,13 +2,13 @@
 
 <html lang="en">
   <head>
-    <title>Globe Bank</title>
+    <title>Goliath National Bank</title>
     <meta charset="utf-8">
   </head>
 
   <body>
 
-  	<h1>Globe Bank Coming soon</h1>
+  	<h1>GNB Coming soon</h1>
 
   </body>
 </html>
