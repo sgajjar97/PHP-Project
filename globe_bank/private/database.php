cfccf51ae7e7124a06ff2e28da49ab9000d50f95
Shail Gajjar <sgajjar97@gmail.com>
2019-10-16 15:33:47 -0700

    Add files via upload

--- globe_bank/private/database.php
+++ globe_bank/private/database.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
-require_once('db_credentials.php');
+  require_once('db_credentials.php');
 
-function db_connect(){
-	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-	confirm_db_connect();
-	return $connection;
-}
+  function db_connect() {
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    confirm_db_connect();
+    return $connection;
+  }
 
-function db_disconnect($connection){
-	if(isset($connection)){
-		mysqli_close($connection);
-	}
-}
+  function db_disconnect($connection) {
+    if(isset($connection)) {
+      mysqli_close($connection);
+    }
+  }
 
-function confirm_db_connect() {
+  function confirm_db_connect() {
     if(mysqli_connect_errno()) {
       $msg = "Database connection failed: ";
       $msg .= mysqli_connect_error();
@@ -23,7 +23,7 @@ function confirm_db_connect() {
     }
   }
 
-function confirm_result_set($result_set) {
+  function confirm_result_set($result_set) {
     if (!$result_set) {
     	exit("Database query failed.");
     }
