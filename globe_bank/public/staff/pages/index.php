4863231d9f69c3837e7cb5caae923ab6c2d8acfd
Shail Gajjar <sgajjar97@gmail.com>
2019-10-16 15:38:28 -0700

    Add files via upload

--- globe_bank/public/staff/pages/index.php
+++ globe_bank/public/staff/pages/index.php
@@ -1,12 +1,15 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 <?php
-  $pages = [
-    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'Globe Bank'],
-    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'History'],
-    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Leadership'],
-    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Contact Us'],
-  ];
+
+$page_set = find_all_pages();
+
+  // $pages = [
+  //   ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'Globe Bank'],
+  //   ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'History'],
+  //   ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Leadership'],
+  //   ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Contact Us'],
+  // ];
 ?>
 
 <?php $page_title = 'Pages'; ?>
@@ -31,9 +34,10 @@
         <th>&nbsp;</th>
   	  </tr>
 
-      <?php foreach($pages as $page) { ?>
+      <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
         <tr>
           <td><?php echo h($page['id']); ?></td>
+          <td><?php echo h($page['subject_id']); ?></td>
           <td><?php echo h($page['position']); ?></td>
           <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
     	    <td><?php echo h($page['menu_name']); ?></td>
@@ -43,6 +47,8 @@
     	  </tr>
       <?php } ?>
   	</table>
+
+<?php mysqli_free_result($page_set); ?>
 
   </div>
 
